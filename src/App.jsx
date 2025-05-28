6647caaa2ac8240750f84c7d1d1741169c2a22be
Mohammad Zaid <mohdzaid07091997@gmail.com>
2025-05-28 21:53:28 +0530

    Visual Edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+// Imports
 import { useState } from 'react'
-import './App.css'
 import { InputBox } from './components/index.js'
 import useCurrencyInfo from './hooks/useCurrencyInfo.js'
+import './App.css'
 
 function App() {
-  const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState("usd");
-  const [to, setTo] = useState("eur");
+  // States
+  const [amount, setAmount] = useState();
   const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
 
+  // Custom Hook
   const currencyInfo = useCurrencyInfo(from)
   const options = Object.keys(currencyInfo)
 
+  // Swap Currency Logic
   const swap = () => {
     setFrom(to)
     setTo(from)
@@ -19,6 +23,7 @@ function App() {
     setConvertedAmount(amount)
   }
 
+  // Conversion Logic
   const convert = () => {
     const result = amount * currencyInfo[to];
     setConvertedAmount(Number(result.toFixed(2)));
@@ -28,8 +33,8 @@ function App() {
   return (
     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
       style={{ backgroundImage: `url(https://images.pexels.com/photos/1629172/pexels-photo-1629172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)` }}>
-      <div className='w-full '>
-        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+      <div className='w-full'>
+        <div className='w-full h-full max-w-2xl max-h-xl mx-auto border shadow-md shadow-gray-600 border-gray-600 rounded-lg p-5 backdrop-blur-sm bg-white/10'>
           <form onSubmit={(e) => {
             e.preventDefault()
             convert()
@@ -46,7 +51,7 @@ function App() {
             </div>
             <div className='relative w-full h-0.5'>
               <button
-                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 cursor-pointer border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white shadow-md shadow-blue-950 rounded-lg px-2 py-0.5'
                 onClick={swap}
               >Swap</button>
             </div>
@@ -62,7 +67,7 @@ function App() {
             </div>
             <button
               type='submit'
-              className='w-full bg-blue-600 cursor-pointer text-white px-4 py-3 rounded-lg'
+              className='w-full mt-4 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white px-4 py-3 shadow-md shadow-blue-950 rounded-lg'
             >Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
           </form>
         </div>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React , {useId} from 'react'
+import {useId} from 'react'
 
 function InputBox({
     label, 
@@ -14,13 +14,15 @@ function InputBox({
 }) {
     const id = useId()
   return (
-    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+    <div className={`bg-white p-3 gap-2 rounded-lg text-sm flex ${className}`}>
         <div className='w-1/2'>
-            <label htmlFor = {id}  className='text-black/40 mb-2 inline-block'> {label} </label>
+            <label htmlFor = {id}  className='text-black/80 mb-2 inline-block'> {label} </label>
             <input
             id={id}
             type='number'
-            className='outline-none w-full bg-transparent py-1.5 '
+            min="0"
+            step="any"
+            className='outline-none w-full rounded-lg m-1 p-2 border-1 border-gray-500 bg-gray-300 '
             placeholder='Amount'
             disabled={amountDisabled}
             value={amount}
@@ -28,8 +30,8 @@ function InputBox({
             />
         </div>
         <div className='w-1/2 flex flex-wrap justify-end text-right'>
-            <p className='text-black/40 mb-2 w-full'> Currency Type</p>
-            <select className='rounded-lg px-1 py-1  bg-gray-100 cursor-pointer  outline-none'
+            <p className='text-black/80 mb-2 w-full'> Currency Type</p>
+            <select className='rounded-lg px-1 p-2 m-1 w-full bg-gray-300 cursor-pointer border-1 border-gray-500 outline-none'
             value={selectedCurrency}
             onChange = {(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
             disabled={currencyDisabled}
